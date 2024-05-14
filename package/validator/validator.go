0471d283037b60ad9eb5cd5f972f1515c38e2253
qincz <1757891823@qq.com>
2024-05-14 18:51:51 +0800

    update validator

--- package/validator/validator.go
+++ package/validator/validator.go
@@ -3,19 +3,15 @@ package validator
 import (
 	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"strings"
+	"zero/package/http"
 )
 
 var (
 	validate *validator.Validate
 )
-
-type Param struct {
-	Field string `json:"field"`
-	Value string `json:"value"`
-	Rule  string `json:"rule"`
-}
 
 // init 创建一个验证器实例 初始化翻译器
 func init() {
@@ -23,24 +19,20 @@ func init() {
 }
 
 // ValidateStruct 验证结构体
-func ValidateStruct(s interface{}) bool {
+func ValidateStruct(c *gin.Context, s interface{}) {
 	err := validate.Struct(s)
 	if err != nil {
 		if _, ok := err.(*validator.InvalidValidationError); ok {
-			fmt.Sprintf("参数验证失败:%s", err)
-			//response.ValidationError(c, fmt.Sprintf("参数验证失败:%s", err))
-			return false
+			http.Alert400(c, http.StatusBadRequest, fmt.Sprintf("参数验证失败:%s", err))
+			return
 		}
 		for _, e := range err.(validator.ValidationErrors) {
-			fmt.Sprintf("参数 %s 验证失败，错误原因为：%s %s ", strings.ToLower(e.StructNamespace()), e.Tag(), e.Param())
-			//response.ValidationError(
-			//	c,
-			//	fmt.Sprintf("参数 %s 验证失败，错误原因为：%s %s ", strings.ToLower(e.StructNamespace()), e.Tag(), e.Param()),
-			//)
-			return false
+			http.Alert400(c, http.StatusBadRequest,
+				fmt.Sprintf("参数 %s 验证失败，错误原因为：%s %s ", strings.ToLower(e.StructNamespace()), e.Tag(), e.Param()))
+			return
 		}
 	}
-	return true
+	return
 }
 
 // ValidateStructWithOutContext 验证结构体
@@ -57,17 +49,4 @@ func ValidateStructWithOutContext(s interface{}) (success bool, err error) {
 		}
 	}
 	return true, nil
-}
-
-// ValidateVariable 验证变量
-func ValidateVariable(params []Param) bool {
-	for _, param := range params {
-		err := validate.Var(param.Value, param.Rule)
-		if err != nil {
-			fmt.Sprintf("参数 %s 验证失败，错误原因为：%s", param.Field, err)
-			//response.ValidationError(c, fmt.Sprintf("参数 %s 验证失败，错误原因为：%s", param.Field, err))
-			return false
-		}
-	}
-	return true
 }
